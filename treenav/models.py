16f9eb385a51f49bc3ac4202d8780c2e38bbf9e8
Nan <ringemup@gmail.com>
2011-09-30 12:06:07 -0400

    allow menu item order from 0 to 100

--- treenav/models.py
+++ treenav/models.py
@@ -91,18 +91,7 @@ class MenuItemManager(models.Manager):
 
     
 class MenuItem(models.Model):
-    ORDER_CHOICES = (
-        (0, 0),
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4),
-        (5, 5),
-        (6, 6),
-        (7, 7),
-        (8, 8),
-        (9, 9),
-    )
+    ORDER_CHOICES = ((i, i) for i in range(0, 100))
     
     parent = models.ForeignKey(
         'MenuItem',
